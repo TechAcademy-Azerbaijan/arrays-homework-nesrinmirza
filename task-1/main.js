cac350bc0485f5829c58a00eb51eb167684da5e3
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-06-28 09:21:30 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -9,9 +9,8 @@ prompt.get('input', function (err, result) {
     Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
     */
     var input = result.input; // "5,2 4 7 12 9"
-    var input_data = input.split(','); // ['5', '2 4 7 12 9']
-    var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var array = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    var array = input.split(','); // ['5', '2 4 7 12 9']
+    
     for(let i=0;i<array.length;i++) {
 
         for(let j=i+1;j<array.length;j++) {
