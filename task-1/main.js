66f5bc982a28058e485242f5a3446fc231b09829
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-06-28 06:57:29 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,22 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var input_data = input.split(','); // ['5', '2 4 7 12 9']
+    var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
+    var array = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    for(let i=0;i<array.length;i++) {
+
+        for(let j=i+1;j<array.length;j++) {
+            if(array[i]===array[j]) {
+                array.splice(j,1);
+            }
+        }
+    }
+    console.log(array.length);
+ });
